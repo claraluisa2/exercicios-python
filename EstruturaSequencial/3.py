0a47abaff4eafcc467988087e0ad492375f7b21c
Clara Luisa <claraluisa2@gmail.com>
2024-11-14 13:47:07 -0300

    dia 1

---
+++ EstruturaSequencial/3.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite um numero: "))
+
+soma = num1 + num2
+
+print(soma)
+print("A soma é", num1+num2)
+print(f"A soma é {num1+num2}")
---
+++ EstruturaSequencial/4.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite uma nota: "))
+nota2 = float(input("Digite uma nota: "))
+nota3 = float(input("Digite uma nota: "))
+nota4 = float(input("Digite uma nota: "))
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print(f"A media e {media}")
