555998eabbdbe63b9f1bd5d9d7a475cc14f8a546
Clara Luisa <claraluisa2@gmail.com>
2024-11-23 10:14:36 -0300

    "dia 2"

---
+++ Funcoes/1.py
@@ -0,0 +1,6 @@
+def exe1(n):
+    for i in range(1, n + 1):
+        print((str(i) + " ") * i)
+
+
+exe1(5)
---
+++ Funcoes/2.py
@@ -0,0 +1,9 @@
+def exe2(n):
+    for i in range(1, n + 1):
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+
+
+
+exe2(5)
